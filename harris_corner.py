a854f78b1a8d4c6d808c4939740cca5c16e879f3
sean <x7chen@126.com>
2018-04-02 15:05:01 +0800

    格式化

--- harris_corner.py
+++ harris_corner.py
@@ -1,21 +1,21 @@
-# -*- coding:utf-8 -*-  
-__author__ = 'Sean'  
-  
-import cv2  
-import numpy as np  
+# -*- coding:utf-8 -*-
+__author__ = 'Sean'
+
+import cv2
+import numpy as np
 import sys
-  
-img = cv2.imread(sys.argv[1])  
-  
-gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)  
-gray = np.float32(gray)  
-  
-dst = cv2.cornerHarris(gray,2,3,0.05)  
-  
-# dst = cv2.dilate(dst,None)  
-  
-img[dst>0.015*dst.max()] = [0,0,255]  
-  
-cv2.imshow("harris_points", img)  
-if cv2.waitKey(0) & 0xff == 27:  
-    cv2.destroyAllWindows()  
+
+img = cv2.imread(sys.argv[1])
+
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+gray = np.float32(gray)
+
+dst = cv2.cornerHarris(gray, 2, 3, 0.05)
+
+# dst = cv2.dilate(dst,None)
+
+img[dst > 0.015 * dst.max()] = [0, 0, 255]
+
+cv2.imshow("harris_points", img)
+if cv2.waitKey(0) & 0xff == 27:
+    cv2.destroyAllWindows()
